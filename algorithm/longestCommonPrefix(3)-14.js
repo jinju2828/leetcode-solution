e2e124c69ef94c16e64e6a6be7c1ed06c4c39f6c
jinju2828 <jinju2828@gmail.com>
2019-08-08 00:30:07 -0700

    14. shorter version

---
+++ algorithm/longestCommonPrefix(3)-14.js
@@ -0,0 +1,20 @@
+function longestCommonPrefix(strs) {
+    if (!strs.length) return "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let str of strs) {
+            console.log("str?",str);
+            console.log(str[i]);
+            console.log(i);
+            if (str[i] !== strs[0][i]) {
+                return str.slice(0, i);
+            }
+        }
+    }
+
+    return strs[0];
+}
+
+var res = longestCommonPrefix(["flo","flww","fldd"]);
+// var res = longestCommonPrefix([""]);
+console.log(res);
