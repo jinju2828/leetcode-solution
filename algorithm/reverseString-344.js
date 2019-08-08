8e1e279804490832dd8be413e98045b1bbd959ff
jinju2828 <jinju2828@gmail.com>
2019-08-08 15:23:46 -0700

    344. not allocating extra array

--- algorithm/reverseString-344.js
+++ algorithm/reverseString-344.js
@@ -1,6 +1,14 @@
-var reverseString = function(s) {
-    return s.reverse();
-};
+// var reverseString = function(s) {
+//     return s.reverse();
+// };
+
+var reverseString = function (s) {
+    for(var i = 0, j=s.length-1; i<j; i++, j--){
+        const x = s[i]; const y = s[j];
+        s[i] = y; s[j] = x;
+    }
+    return s;
+}
 
 var res =  reverseString(["h","e","l","l","o"]);
 console.log(res);
